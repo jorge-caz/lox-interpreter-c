98c4e3e39f3626d8761fa0ff074a76605bd88220
Jorge <jorge_lc@mit.edu>
2025-01-28 02:56:24 -0500

    add numbers

--- src/main.c
+++ src/main.c
@@ -116,7 +116,7 @@ int main(int argc, char *argv[]) {
                     char temp = *notnum; *notnum = '\0';
                     float num = strtof(stri, NULL);
                     if (is_integer)
-                    printf("NUMBER %s %s.0", stri, stri);
+                    printf("NUMBER %s %s.0\n", stri, stri);
                     else
                     printf("NUMBER %s %.7g\n", stri, num);
                     *notnum = temp;
