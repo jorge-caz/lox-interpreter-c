a2da6499331e01061d19be9e18a0547e20191c13
Jorge <jorge_lc@mit.edu>
2025-01-29 19:35:22 -0500

    fix bugs

--- src/scanner.c
+++ src/scanner.c
@@ -11,6 +11,7 @@ Token create_token(TokenType type, const char* lexeme, int line) {
     token.lexeme= lexeme;
     token.line = line;
     n++;
+    printf("we got this as our %nth token: %s\n", n, token.lexeme);
     
     return token;
 }
